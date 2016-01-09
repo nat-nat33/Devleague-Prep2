f1a14ecfcf2960ceacc2dd7e61add79bd33321e6
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-01-08 22:11:45 -1000

    isGreaterThan Man

--- exercises.js
+++ exercises.js
@@ -11,3 +11,17 @@ console.log('youGetTaco', myAction, youGetTaco(myAction));
 var myOtherAction = 'drink';
 console.log('youGetTaco', myOtherAction, youGetTaco(myOtherAction));
 
+function isNumberGreaterThan(first, second){
+	if(first>second){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+var myFirst = 9;
+var mySecond = 10;
+var firstMoreThan = isNumberGreaterThan(myFirst, mySecond);
+console.log('isNumberGreaterThan', myFirst, mySecond, firstMoreThan);
+
+
