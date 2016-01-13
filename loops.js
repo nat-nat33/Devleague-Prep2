010966f7ca510558cedcdd79fc70495d605cd395
Juan Jose Gonzalez Macias <JuanJo@MacBook-Pro-de-Juan-Jose-Gonzalez-Macias.local>
2016-01-12 18:20:35 -1000

    get loopy presidents

---
+++ loops.js
@@ -0,0 +1,7 @@
+var presidents= ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+for(var i = 0; i<presidents.length; i++){
+	console.log('value of i is:',i);
+	console.log('value of index is:' + presidents[i]);
+}
+
